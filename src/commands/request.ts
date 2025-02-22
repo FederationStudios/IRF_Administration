bbb506bcbc2b3398edc2faffe4fedb68cbb5edf7
Tavi <66774833+totallytavi@users.noreply.github.com>
2025-02-21 18:22:57 -0600

    fix: update request division check condition
    * Add a strict check for division on both conditionals

--- src/commands/request.ts
+++ src/commands/request.ts
@@ -54,8 +54,8 @@ export async function run(
   if (interaction.guild.id != '466432774182666240' && options.getString('division') === 'Imperial Guard') {
     interactionEmbed(3, 'You must request Imperial Guard from their server', interaction);
     return;
-  } else if (interaction.guild.id != discord.mainServer) {
-    interactionEmbed( 3, 'You must request this division in the Federation Network server', interaction);
+  } else if (interaction.guild.id != discord.mainServer && options.getString('division') != 'Imperial Guard') {
+    interactionEmbed(3, 'You must request this division in the Federation Network server', interaction);
     return;
   }
   const division = options.getString('division');
